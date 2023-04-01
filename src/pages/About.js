066bf30a83f53c347f7238334b0c61dce3b4af81
Kundanagrawalofficial <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-04-01 10:01:31 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout.js";
-import "./About.css";
 
 const About = () => {
   return (
@@ -13,24 +12,22 @@ const About = () => {
             style={{ width: "100%" }}
           />
         </div>
-        <div className="col-md-4 about-text">
+        <div className="col-md-6">
+          <h1>Welcome to Mobile Point!</h1>
           <p>
-            Welcome to mobilepoint.in, your one-stop-shop for all the latest and greatest gadgets!
+            We're your one-stop-shop for all the latest and greatest gadgets! At Mobile Point, we're passionate about technology and dedicated to providing you with the best selection of gadgets at competitive prices. 
           </p>
           <p>
-            At mobilepoint.in, we're passionate about technology and are dedicated to providing you with the best selection of gadgets at competitive prices. Whether you're looking for the latest smartphone, a sleek laptop, or a cutting-edge smartwatch, we've got you covered.
+            Whether you're looking for the latest smartphone, a sleek laptop, or a cutting-edge smartwatch, we've got you covered. Our team of tech experts is constantly scouring the market for the newest and most innovative gadgets, so you can be sure that our product selection is always up-to-date. 
           </p>
           <p>
-            Our team of tech experts is constantly scouring the market for the newest and most innovative gadgets, so you can be sure that our product selection is always up-to-date. We pride ourselves on offering a wide variety of products from top brands, as well as lesser-known up-and-coming brands that we believe in.
+            We pride ourselves on offering a wide variety of products from top brands, as well as lesser-known up-and-coming brands that we believe in. And we know that shopping for gadgets can be overwhelming, which is why we're committed to providing you with exceptional customer service.
           </p>
           <p>
-            We know that shopping for gadgets can be overwhelming, which is why we're committed to providing you with exceptional customer service. Our friendly and knowledgeable support team is always ready to answer your questions and help you find the perfect product for your needs.
+            Our friendly and knowledgeable support team is always ready to answer your questions and help you find the perfect product for your needs. We believe in transparency and honesty, which is why we provide detailed product descriptions, customer reviews, and competitive pricing on all of our products. Plus, with our fast and reliable shipping, you can start enjoying your new gadgets in no time.
           </p>
           <p>
-            We believe in transparency and honesty, which is why we provide detailed product descriptions, customer reviews, and competitive pricing on all of our products. Plus, with our fast and reliable shipping, you can start enjoying your new gadgets in no time.
-          </p>
-          <p>
-            Thank you for choosing mobilepoint.in for all your gadget needs. We're excited to be a part of your tech journey!
+            Thank you for choosing Mobile Point for all your gadget needs. We're excited to be a part of your tech journey!
           </p>
         </div>
       </div>
