623b9e7798de65779c926583c08315cae2e0fa31
Kundanagrawalofficial <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-04-01 09:46:26 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import Layout from "./../components/Layout/Layout.js";
 
 const About = () => {
   return (
-    <Layout title={`About us - Album seller`}>
+    <Layout title={`About us -  Mobile Point`}>
       <div className="row contactus ">
         <div className="col-md-6 ">
           <img
@@ -14,12 +14,17 @@ const About = () => {
         </div>
         <div className="col-md-4">
           <p className="text-justify mt-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
-            perferendis eius temporibus dicta blanditiis doloremque explicabo
-            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
-            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
-            commodi illum quidem neque tempora nam.
+          Welcome to mobilepoint.in, your one-stop-shop for all the latest and greatest gadgets!
+
+At mobilepoint.in, we're passionate about technology and are dedicated to providing you with the best selection of gadgets at competitive prices. Whether you're looking for the latest smartphone, a sleek laptop, or a cutting-edge smartwatch, we've got you covered.
+
+Our team of tech experts is constantly scouring the market for the newest and most innovative gadgets, so you can be sure that our product selection is always up-to-date. We pride ourselves on offering a wide variety of products from top brands, as well as lesser-known up-and-coming brands that we believe in.
+
+We know that shopping for gadgets can be overwhelming, which is why we're committed to providing you with exceptional customer service. Our friendly and knowledgeable support team is always ready to answer your questions and help you find the perfect product for your needs.
+
+We believe in transparency and honesty, which is why we provide detailed product descriptions, customer reviews, and competitive pricing on all of our products. Plus, with our fast and reliable shipping, you can start enjoying your new gadgets in no time.
+
+Thank you for choosing mobilepoint.in for all your gadget needs. We're excited to be a part of your tech journey!
           </p>
         </div>
       </div>
