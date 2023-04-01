9d1423ad1c6921c53e7e0233c90ee8066f655d72
Kundanagrawalofficial <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-04-01 10:06:32 +0530

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout";
 import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
+
 const Contact = () => {
   return (
     <Layout>
@@ -15,17 +16,16 @@ const Contact = () => {
         <div className="col-md-4">
           <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
           <p className="text-justify mt-2">
-            any query and info about prodduct feel free to call anytime we 24X7
-            vaialible
+            We're here to help with any questions or concerns you may have. Don't hesitate to reach out to us using one of the methods below.
           </p>
           <p className="mt-3">
-            <BiMailSend /> : www.help@ecommerceapp.com
+            <BiMailSend /> : mobilepointcustomercare@gmail.com
           </p>
           <p className="mt-3">
-            <BiPhoneCall /> : 012-3456789
+            <BiPhoneCall /> : +91 7050653832
           </p>
           <p className="mt-3">
-            <BiSupport /> : 1800-0000-0000 (toll free)
+            <BiSupport /> : +91 7050653832
           </p>
         </div>
       </div>
