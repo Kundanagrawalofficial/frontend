9218c8824b72ba94b36131bf87d119bc94353750
Kundanagrawalofficial <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-04-01 09:55:28 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,56 +1,64 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout.js";
-import "./About.css"
+
 const About = () => {
   return (
-    <Layout title={`About us - Mobile Point`}>
-      <div className={`row ${styles.container}`}>
+    <Layout title={`About us -  Mobile Point`}>
+      <div className="row contactus">
         <div className="col-md-6">
           <img
             src="/images/about.jpeg"
             alt="contactus"
-            className={styles.image}
+            style={{ width: "100%" }}
           />
         </div>
-        <div className={`col-md-4 ${styles.textContainer}`}>
-          <h2 className={styles.heading}>Welcome to Mobile Point</h2>
-          <p className={styles.text}>
-            We are your one-stop-shop for all the latest and greatest gadgets!
+        <div className="col-md-4 about-text">
+          <p>
+            Welcome to mobilepoint.in, your one-stop-shop for all the latest and greatest gadgets!
           </p>
-          <p className={styles.text}>
-            At Mobile Point, we are passionate about technology and dedicated to
-            providing you with the best selection of gadgets at competitive
-            prices.
+          <p>
+            At mobilepoint.in, we're passionate about technology and are dedicated to providing you with the best selection of gadgets at competitive prices. Whether you're looking for the latest smartphone, a sleek laptop, or a cutting-edge smartwatch, we've got you covered.
           </p>
-          <p className={styles.text}>
-            Our team of tech experts is constantly scouring the market for the
-            newest and most innovative gadgets, so you can be sure that our
-            product selection is always up-to-date.
+          <p>
+            Our team of tech experts is constantly scouring the market for the newest and most innovative gadgets, so you can be sure that our product selection is always up-to-date. We pride ourselves on offering a wide variety of products from top brands, as well as lesser-known up-and-coming brands that we believe in.
           </p>
-          <p className={styles.text}>
-            We pride ourselves on offering a wide variety of products from top
-            brands, as well as lesser-known up-and-coming brands that we believe
-            in.
+          <p>
+            We know that shopping for gadgets can be overwhelming, which is why we're committed to providing you with exceptional customer service. Our friendly and knowledgeable support team is always ready to answer your questions and help you find the perfect product for your needs.
           </p>
-          <p className={styles.text}>
-            We understand that shopping for gadgets can be overwhelming, which
-            is why we are committed to providing you with exceptional customer
-            service. Our friendly and knowledgeable support team is always ready
-            to answer your questions and help you find the perfect product for
-            your needs.
+          <p>
+            We believe in transparency and honesty, which is why we provide detailed product descriptions, customer reviews, and competitive pricing on all of our products. Plus, with our fast and reliable shipping, you can start enjoying your new gadgets in no time.
           </p>
-          <p className={styles.text}>
-            We believe in transparency and honesty, which is why we provide
-            detailed product descriptions, customer reviews, and competitive
-            pricing on all of our products. Plus, with our fast and reliable
-            shipping, you can start enjoying your new gadgets in no time.
-          </p>
-          <p className={styles.text}>
-            Thank you for choosing Mobile Point for all your gadget needs. We
-            are excited to be a part of your tech journey!
+          <p>
+            Thank you for choosing mobilepoint.in for all your gadget needs. We're excited to be a part of your tech journey!
           </p>
         </div>
       </div>
+      <style jsx>{`
+        .contactus {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 50px;
+          margin: 50px 0;
+        }
+
+        .about-text {
+          background-color: #f8f8f8;
+          padding: 20px;
+          border-radius: 10px;
+          box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
+        }
+
+        @media only screen and (max-width: 768px) {
+          .contactus {
+            flex-direction: column;
+          }
+
+          .col-md-6 {
+            margin-bottom: 20px;
+          }
+        }
+      `}</style>
     </Layout>
   );
 };
