d2c43302977a9074377f9d7e9da36fbb2fd6169f
Kundanagrawalofficial <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-04-01 09:46:47 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,33 +4,35 @@ import Layout from "./../components/Layout/Layout.js";
 const About = () => {
   return (
     <Layout title={`About us -  Mobile Point`}>
-      <div className="row contactus ">
-        <div className="col-md-6 ">
+      <div className="row contactus">
+        <div className="col-md-6">
           <img
             src="/images/about.jpeg"
             alt="contactus"
             style={{ width: "100%" }}
           />
         </div>
-        <div className="col-md-4">
-          <p className="text-justify mt-2">
-          Welcome to mobilepoint.in, your one-stop-shop for all the latest and greatest gadgets!
-
-At mobilepoint.in, we're passionate about technology and are dedicated to providing you with the best selection of gadgets at competitive prices. Whether you're looking for the latest smartphone, a sleek laptop, or a cutting-edge smartwatch, we've got you covered.
-
-Our team of tech experts is constantly scouring the market for the newest and most innovative gadgets, so you can be sure that our product selection is always up-to-date. We pride ourselves on offering a wide variety of products from top brands, as well as lesser-known up-and-coming brands that we believe in.
-
-We know that shopping for gadgets can be overwhelming, which is why we're committed to providing you with exceptional customer service. Our friendly and knowledgeable support team is always ready to answer your questions and help you find the perfect product for your needs.
-
-We believe in transparency and honesty, which is why we provide detailed product descriptions, customer reviews, and competitive pricing on all of our products. Plus, with our fast and reliable shipping, you can start enjoying your new gadgets in no time.
-
-Thank you for choosing mobilepoint.in for all your gadget needs. We're excited to be a part of your tech journey!
+        <div className="col-md-6">
+          <h1>Welcome to Mobile Point!</h1>
+          <p>
+            We're your one-stop-shop for all the latest and greatest gadgets! At Mobile Point, we're passionate about technology and dedicated to providing you with the best selection of gadgets at competitive prices. 
+          </p>
+          <p>
+            Whether you're looking for the latest smartphone, a sleek laptop, or a cutting-edge smartwatch, we've got you covered. Our team of tech experts is constantly scouring the market for the newest and most innovative gadgets, so you can be sure that our product selection is always up-to-date. 
+          </p>
+          <p>
+            We pride ourselves on offering a wide variety of products from top brands, as well as lesser-known up-and-coming brands that we believe in. And we know that shopping for gadgets can be overwhelming, which is why we're committed to providing you with exceptional customer service.
+          </p>
+          <p>
+            Our friendly and knowledgeable support team is always ready to answer your questions and help you find the perfect product for your needs. We believe in transparency and honesty, which is why we provide detailed product descriptions, customer reviews, and competitive pricing on all of our products. Plus, with our fast and reliable shipping, you can start enjoying your new gadgets in no time.
+          </p>
+          <p>
+            Thank you for choosing Mobile Point for all your gadget needs. We're excited to be a part of your tech journey!
           </p>
         </div>
       </div>
     </Layout>
   );
-  
 };
 
 export default About;
