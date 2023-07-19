422b90c36e187e67de96694ee953d3ae6b3f35e8
Kundan Agrawal <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-07-19 15:51:06 +0530

    Update Footer.js

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -9,6 +9,10 @@ const Footer = () => {
         <Link to="/about">About</Link> | <Link to="/contact">Contact</Link> | <Link to="/policy">Privacy Policy</Link>
         {/* Add the Terms and Conditions link below */}
         | <a href="https://merchant.razorpay.com/policy/MFbwSdsf69dpyL/terms" target="_blank" rel="noopener noreferrer">Terms and Conditions</a>
+        {/* Add the Shipping and Delivery link below */}
+        | <a href="https://merchant.razorpay.com/policy/MFbwSdsf69dpyL/shipping" target="_blank" rel="noopener noreferrer">Shipping and Delivery</a>
+        {/* Add the Refund and Cancellation link below */}
+        | <a href="https://merchant.razorpay.com/policy/MFbwSdsf69dpyL/refund" target="_blank" rel="noopener noreferrer">Refund and Cancellation</a>
       </p>
     </div>
   );
