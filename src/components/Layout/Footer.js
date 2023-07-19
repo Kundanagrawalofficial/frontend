8a1ab622adf1bee3b8e61e7a20b42f7cab4c9df4
Kundan Agrawal <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-07-19 15:42:03 +0530

    Update Footer.js

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
   return (
     <div className='footer'>
       <h4 className='text-center'>All Right Reserved &copy; Seller</h4>
       <p className="text-center mt-3">
-        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
-        <Link to="/policy">Privacy Policy</Link>
+        <Link to="/about">About</Link> | <Link to="/contact">Contact</Link> | <Link to="/policy">Privacy Policy</Link>
+        {/* Add the Terms and Conditions link below */}
+        | <a href="https://merchant.razorpay.com/policy/MFbwSdsf69dpyL/terms" target="_blank" rel="noopener noreferrer">Terms and Conditions</a>
       </p>
+    </div>
+  );
+};
 
-      </div>
-  )
-}
-
-export default Footer
+export default Footer;
